b64e1cf51d9ca3a2bf88f5c027c6411d70a70ed4
Heltborg_Caleb <heltc415@hsd.k12.or.us>
2018-12-21 09:58:07 -0800

    initial files

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+       
+    void OnCollisionEnter (Collision collisioninfo)
+    {
+        if(collisioninfo.collider.name == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    //refers to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+	//"Fixed"Update because we are messing with physics
+	void FixedUpdate () {
+        //add forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
